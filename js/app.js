2f1a4231c9ee76766cc8080ac610dcbb7ceba9a8
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-07-08 21:18:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
 */
 const navSectionNames = [];
 let navlist, sects, sectNames;
-
+let oldActiveSection;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -34,6 +34,20 @@ function fixLength(string){
 	return str;
 }
 
+
+function isInsideBoundaries(elem) {
+    const boundary = elem.getBoundingClientRect();
+	const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+	const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+	
+	const topLimit = viewportHeight * 0.2;
+	const bottomLimit = viewportWidth *0.8;
+	const isBounded = boundary.top >= 0 &&
+					  boundary.left >= 0 &&
+					  boundary.bottom <= viewportHeight;
+					  boundary.right <= viewportWidth;
+    return isBounded;
+}
 
 /**
  * End Helper Functions
@@ -62,6 +76,8 @@ document.addEventListener('DOMContentLoaded', function(){
 	sectNames = document.querySelectorAll('.landing__container h2');
 	populateNavSectionNames();
 	populateNavList();
+	actionOnClick();
+	//actionOnScroll();
 });
 
 // Populate navSectionNames 
@@ -81,7 +97,9 @@ function populateNavList(){
 	for(let x=0; x < navSectionNames.length; x++){
 		const liElem = document.createElement('li');
 		const anchor = document.createElement('a');
+		const id = 'section' + (x +1) +'_';
 		anchor.setAttribute('class', 'menu__link');
+		anchor.setAttribute('id', id);
 		anchor.textContent = navSectionNames[x];
 		liElem.appendChild(anchor);
 		navlist.appendChild(liElem);
@@ -89,11 +107,70 @@ function populateNavList(){
 }
 
 // Scroll to section on link click
-function addScrollToClick(){
+function actionOnClick(){
 	navlist.addEventListener('click', function(event){
+		// get section id from the event target (id name is section<section number>_)
+		//so just subtract the underscore and get the referenced section
+		// and its heading after that so we can change its color
+		const sectionId = event.target.getAttribute('id').substring(0,8);
+		const section = document.querySelector('#' +sectionId);
+		const heading = section.firstElementChild.firstElementChild;
+		const currentActiveSection = section;
+		// add active__section class to the section already chosen from the nav bar
+		section.classList.add('active__section');
+		// check for oldActiveSection if it exists and not equal to the currently selected section
+		// to prevent unchecking the currently checked on second successive click ! 
+		if(oldActiveSection && (oldActiveSection !== currentActiveSection.getAttribute('id').substring(7,))){
+			sects[oldActiveSection -1].classList.remove('active__section');
+			sects[oldActiveSection -1].firstElementChild.firstElementChild.style.color = '#fff';
+		}
+		// reassign(or assign if it's the first click) oldActiveSection variable
+		oldActiveSection = sectionId.substring(7,);
+		// scroll to the section just chosen
+		section.scrollIntoView({behavior: 'smooth'});
+		// change the color of its h2 heading
+		heading.style.color = '#c06c6c';
 		
 	});
 }
+
+function actionOnScroll(){
+	
+	const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+	const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+	
+	const topLimit = viewportHeight * 0.2;
+	const bottomLimit = viewportWidth *0.8;
+	document.addEventListener('scroll', function(event){
+		for(let x=0; x < sects.length; x++){
+			const isOut = isOutOfViewport(sects[x]);
+			
+			if(!out.top){
+				sects[x].classList.add('active__section');
+				sects[x].firstElementChild.firstElementChild.style.color = '#c06c6c';
+			}else{
+				sects[x].remove('active__section');
+				sects[x].firstElementChild.firstElementChild.style.color = '#fff';
+			}
+		}
+	});
+}
+/*
+[{},{},{},{}]
+
+function toggleActive(){
+	for(let x=0; x < sects.length; x++){
+		if(isInsideBoundaries(sects[x])){
+			sects[x].classList.add('active__section');
+			return sects[x];
+		}else{
+			sects[x].classList.remove('active__section');
+			return null;
+		}
+	}
+}
+*/
+
 
 /* 
 to detect a mobile device
@@ -108,7 +185,23 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
 
 */
 
+var isOutOfViewport = function (elem) {
 
+	// Get element's bounding
+	var bounding = elem.getBoundingClientRect();
+
+	// Check if it's out of the viewport on each side
+	var out = {};
+	out.top = bounding.top < 0;
+	out.left = bounding.left < 0;
+	out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+	out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+	out.any = out.top || out.left || out.bottom || out.right;
+	out.all = out.top && out.left && out.bottom && out.right;
+
+	return out;
+
+};
 
 
 // Set sections as active
